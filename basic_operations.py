3eb83125c5b1add289436c8784ef3b7065494c3f
xilak <natanzerihun101@gmail.com>
2025-07-18 14:47:29 +0300

    add basic operations script

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+number1=10
+number2=5
+
+sum = number1 + number2
+differnce = number1 - number2
+product = number1 * number2
+
+print(f"sum of {number1} and {number2} is " + str(sum))
+print(f"differnce of {number1} and {number2} is " + str(differnce))
+print(f"product of {number1} and {number2} is " + str(product))
