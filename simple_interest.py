a52724bd72b0428c58e5cffb2b7f3564d50546bb
xilak <natanzerihun101@gmail.com>
2025-07-18 15:50:36 +0300

    add simple_interest

--- simple_interest.py
+++ simple_interest.py
@@ -1,14 +1,7 @@
-'''
-Define three variables in this file:
-principal with a value of 1000 (representing $1000),
-rate with a value of 0.05 (representing 5% annual interest rate),
-time with a value of 3 (representing 3 years).
-Calculate the simple interest using the formula provided and store the result in a variable named interest.
-Print the calculated interest in a format: The simple interest is: [interest].
-'''
-principal = 1000#$
-rate = 0.05#5%
-time = 3#year
+
+principal = 1000
+rate = 0.05
+time = 3
 
 interest = principal * rate * time
 
