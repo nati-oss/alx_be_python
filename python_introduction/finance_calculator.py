1b7435dd6fffe3aa6d437f23b9bd4651351b2095
xilak <natanzerihun101@gmail.com>
2025-07-29 20:56:04 +0300

    fixing bugs

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,9 @@
 # finance_calculator.py
 
-income = float(input("Enter your monthly income: "))
-expenses = float(input("Enter your total monthly expenses: "))
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
 
-monthly_savings = income - expenses
+monthly_savings = monthly_income - monthly_expenses
 
 # Simple interest projection
 annual_savings = monthly_savings * 12
